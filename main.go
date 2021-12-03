8a72312bce92cd52e1f792a4ac2a02ad286fce99
andrzej.zmudzinski <jendrusha@gmail.com>
2021-12-03 18:53:28 +0100

    feat: add main cmd

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"log"
+
+	"github.com/deseco-tech/oapi-resolver/internal/resolver"
 )
 
 func main() {
@@ -10,4 +13,13 @@ func main() {
 	flag.StringVar(&inputFile, "i", "", "input file path")
 	flag.StringVar(&outputFile, "o", "", "output file path")
 	flag.Parse()
+
+	processor, err := resolver.New(inputFile, outputFile)
+	if err != nil {
+		log.Fatalf("unable to create manager: %v", err)
+	}
+
+	if err := processor.Combaine(); err != nil {
+		log.Fatalf("unable to combine spec: %v", err)
+	}
 }
